3f580adfbeebd39ffc5f5309bb01c236c18bd46c
ADETUNJI OPAYELE <lordwhizy@gmail.com>
2015-10-28 13:15:46 +0100

    initail installation

---
+++ config/blog.php
@@ -0,0 +1,14 @@
+<?php
+return [
+  'name' => "L5 Beauty",
+  'title' => "Laravel 5.1 Beauty",
+  'subtitle' => 'A clean blog written in Laravel 5.1',
+  'description' => 'This is my meta description',
+  'author' => 'TeeJaY',
+  'page_image' => 'home-bg.jpg',
+  'posts_per_page' => 10,
+  'uploads' => [
+    'storage' => 'local',
+    'webpath' => '/uploads/',
+  ],
+];
